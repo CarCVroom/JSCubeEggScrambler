61b46345c47969e420632a0ca1873c1de711770f
CarCVroom <bjornarlundm@gmail.com>
2026-03-27 18:24:33 +0100

    Remove WR and simplify betterWR loop
    Delete the old WR() routine (hardcoded moves and many inline waits) and streamline the repeating algorithm flow in betterWR(). Parser calls are now awaited, the second parser timing was adjusted (95 → 70), an explicit x2 rotation and a 1s wait were added before looping, and the initial parser call is awaited as well. These changes simplify the code, fix async timing issues, and centralize the algorithm execution into betterWR().

--- JS/main.js
+++ JS/main.js
@@ -9,59 +9,17 @@ renderer.setAnimationLoop(rotateOnDrag);
 
 await wait(5000)
 
-async function WR() {
-        L(1); B(1); R(2); B(3); R(2); U(2); F(1); D(1); R(2); U(1); R(2); F(2); D(2); R(1); U(1); B(1); L(2);
-        x(3);
-
-
-
-        await wait(2000);
-
-        // 3xxx cross or what ever
-        r(3); await wait(95);
-        U(1); await wait(95);
-        F(1); await wait(95);
-        U(3); await wait(95);
-        r(1); await wait(95);
-        U(3); await wait(95);
-        r(3); await wait(95);
-        U(2); await wait(95);
-        r(3); await wait(95);
-        U(1); await wait(95);
-        r(1); await wait(95);
-
-        // 4th pair
-        R(1); await wait(95);
-        U(2); await wait(95);
-        R(2); await wait(95);
-        U(3); await wait(95);
-        R(1); await wait(95);
-        U(1); await wait(95);
-        R(1); await wait(95);
-        U(2); await wait(95);
-        R(3); await wait(95);
-
-        // OLL >> PLL Skip
-        U(3); await wait(95);
-        F(3); await wait(95);
-        r(1); await wait(95);
-        U(1); await wait(95);
-        R(3); await wait(95);
-        U(3); await wait(95);
-        r(3); await wait(95);
-        F(1); await wait(95);
-        R(1); 
-        x(2);
-        await wait(2000);
-        WR();
-}
 
 async function betterWR() {
-        parser("L B R2 B' R2 U2 F D R2 U R2 F2 D2 R U B L2", 0);
+        await parser("L B R2 B' R2 U2 F D R2 U R2 F2 D2 R U B L2", 0);
 
         await wait(2000);
 
-        parser("x' r' U F U' r U' r' U2 r' U r R U2 R2 U' R U R U2 R' U' F' r U R' U' r' F R", 95)
+        await parser("x' r' U F U' r U' r' U2 r' U r R U2 R2 U' R U R U2 R' U' F' r U R' U' r' F R", 70)
+
+        await wait(1000);
+        await parser("x2");
+        betterWR();
 }
 betterWR()
 

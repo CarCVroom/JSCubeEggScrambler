1999450c9b21e9c6d97eaee81894d1f8d050fdf2
CarCVroom <bjornarlundm@gmail.com>
2026-04-10 10:10:11 +0200

    I think it works, but idk rn
    it should work

--- JS/input.js
+++ JS/input.js
@@ -11,6 +11,7 @@ export async function inputRunner(scramMoves, solMoves) {
         //console.log(movesScramble)
         
         await reverseOldMoves()
+        await runTheBitch(0, false);
         await runTheBitch(0, true);
 
         let loop, tps; 
@@ -88,8 +89,8 @@ export async function inputRunner(scramMoves, solMoves) {
                         // await reverseOldMoves()
                         await runTheBitch(0, true)
                 }
-                console.warn("In if statment")
-                console.log(movesScramble)
+                //console.warn("In if statment")
+                //console.log(movesScramble)
         }
 
         if (!scramMoves && solMoves) {
@@ -177,8 +178,38 @@ async function reverseOldMoves() {
                 }
                 // Bro how am i going to do this shit bro
         })
-        console.warn("MOVESCRAMBLE IN REVERSE OLD MOVES")
-        console.log(movesScramble);
+        movesSolution.reverse()
+        movesSolution.forEach((move, i) => {
+                if (move.firstParam === 1) { 
+                        // Reads the added things and then uses that to do the revese 1 -> 3, 3 -> 1
+                        firstParamVar = 3;
+                } else if (move.firstParam === 3) {
+                        firstParamVar = 1;
+                } else {
+                        firstParamVar = 2;
+                }
+
+                const moveMap = {
+                        R, L, U, D, F, B,
+                        r, l, u, d, f, b,
+                        M, E, S,
+                        x, y, z
+                };
+
+                const moveFn = moveMap[move.moveName];
+
+                if (moveFn) {
+                        const param = firstParamVar;
+                        console.log(`firstParamVar is ${firstParamVar}, and param is ${param}`)
+                        fn = () => moveFn(param);
+                        fn.firstParam = firstParamVar;
+                        fn.moveName = move.moveName;
+                        movesSolution[i] = fn;
+                }
+                // Bro how am i going to do this shit bro
+        })
+        //console.warn("MOVESCRAMBLE IN REVERSE OLD MOVES")
+        //console.log(movesScramble);
         // I thinks it works with scramble
         // NOW I NEED TO DO IT WITH SOLUTION
         // FUCK

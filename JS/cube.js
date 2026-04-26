d10c87a36b9634304ba819c09d470f6e332604d5
CarCVroom <bjornarlundm@gmail.com>
2026-04-26 22:36:12 +0200

    no work pls fixs claude
    IM DOING STUFF AND JAIFJASD IOFJSIOFJSDIOFJ ASDIOFJASDIO FJIOSDFJ ISDF FJIOSDFJASDKLFJ ASDKLFJ FKL;ASDFSDJ KLSDJFAKL ASDJ FKL ASDJFIASDJFIOWE FWEANFIEA WJVIOEAJW

--- JS/cube.js
+++ JS/cube.js
@@ -1,10 +1,10 @@
 const cube = [
-        [0,0,0,0,0,0,0,0,0], // WHITE / U
-        [1,1,1,1,1,1,1,1,1], // ORANGE / L
-        [2,2,2,2,2,2,2,2,2], // GREEN / F
-        [3,3,3,3,3,3,3,3,3], // RED / R
-        [4,4,4,4,4,4,4,4,4], // BLUE / B
-        [5,5,5,5,5,5,5,5,5], // YELLOW / D
+        ['a','a','b','d','WCenter','b','d','c','c'], // WHITE / U
+        ['e','e','f','h','OCenter','f','h','g','g'], // ORANGE / L
+        ['i','i','j','l','GCenter','j','l','k','k'], // GREEN / F
+        ['m','m','n','p','RCenter','n','p','o','o'], // RED / R
+        ['q','q','r','t','BCenter','r','t','s','s'], // BLUE / B
+        ['u','u','v','x','YCenter','v','x','w','w'], // YELLOW / D
 ];      
 // It would be like aabbCENTERccdd
 // First is corner and second is edge
@@ -12,12 +12,14 @@ const cube = [
 
 // NOW WE HAVE ALL SIX SIDES OF A CUBE INDICATED BY COLORS
 let cubePlaying = [...cube];
+const cwMap = [2,5,8,1,4,7,0,3,6];
+let newSide;
 
 function moveR(prime, doubleMove, move) {
 
         let upp = getTheInput(prime, move);
         let times = doubleMove ? 2 : 1;
-        let stickers = sideStickers(move);
+        let { stickers, side }= sideStickers(move);
         
         for(let t = 0; t < times; t++) {
                 for(let i = 0; i < upp.length - 1; i++) {
@@ -25,6 +27,10 @@ function moveR(prime, doubleMove, move) {
                                 [cubePlaying[upp[i]][idx],     cubePlaying[upp[i + 1]][idx]] =
                                 [cubePlaying[upp[i + 1]][idx], cubePlaying[upp[i]][idx]];
                         }
+                        cubePlaying[side].forEach((sticker, i) => {
+                                if (i === 4) return;
+                                newSide[cwMap[i]] = sticker;
+                        })
                 }
         }
         console.log(cubePlaying)
@@ -33,14 +39,14 @@ function doubleArray(arr) {
         return arr = [...arr, ...arr];
 }
 function getTheInput(prime, move) {
-        let R = [5, 4, 0, 2]; // This is a full move im stupid 
-        let L = [5, 2, 0, 4]; // add something and another paramater that makes it so that i can chose moves with one function.
-        let U = [2, 3, 4, 1];
-        let targetArray; 
+        let R = [5, 4, 0, 2]; 
+        let L = [5, 2, 0, 4]; 
+        let U = [2, 3, 4, 1]; // this is the order that we are doing it in
+        let targetArray, targetSideArray; 
 
         switch (move) {
                 case "R":
-                        targetArray = R;                       
+                        targetArray = R;                                         
                         break;
                 case "L":
                         targetArray = L;                       
@@ -50,7 +56,6 @@ function getTheInput(prime, move) {
                         break;
                 default:
                         throw new Error(`Unknown move: "${move}"`);
-                        break;
         }
 
         if (prime) { // A check to see if it's not prime, as current order does a prime move
@@ -61,24 +66,28 @@ function getTheInput(prime, move) {
         return targetArray;
 }
 function sideStickers(move) {
-        let stickers;
+        let stickers, side;
 
         switch (move) {
                 case "R":
-                        stickers = [2,5,8]; 
+                        stickers = [2,5,8]; // These are the stickers on each side we are targeting
+                        side = 3;
                         break;
                 case "L":
                         stickers = [0,3,6]; 
+                        side = 1;
                         break;
                 case "U":
                         stickers = [0,1,2];
-                default:
+                        side = 0;
                         break;
+                default:
+                        throw new Error(`Unknown move: "${move}"`);
         }
 
-        return stickers;
+        return { stickers, side };
 }
-moveR(false, true, "U");
+moveR(false, false, "R");
 //moveR(false, false, "R");
 
 // oh no i can do old pochman lol

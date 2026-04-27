3d42c9da6f0ef28ab03d68ed7b2bd353b9195206
CarCVroom <bjornarlundm@gmail.com>
2026-04-27 11:28:07 +0200

    I made it work byt without f and b so you will always to ZZ

--- JS/cube.js
+++ JS/cube.js
@@ -1,10 +1,13 @@
+import { printCube } from './utils.js'
+
 const cube = [
-        ['a','a','b','d','WCenter','b','d','c','c'], // WHITE / U
-        ['e','e','f','h','OCenter','f','h','g','g'], // ORANGE / L
-        ['i','i','j','l','GCenter','j','l','k','k'], // GREEN / F
-        ['m','m','n','p','RCenter','n','p','o','o'], // RED / R
-        ['q','q','r','t','BCenter','r','t','s','s'], // BLUE / B
-        ['u','u','v','x','YCenter','v','x','w','w'], // YELLOW / D
+        ['aW','aW','bW','dW','WC','bW','dW','cW','cW'], // WHITE / U
+        ['eO','eO','fO','hO','OC','fO','hO','gO','gO'], // ORANGE / L
+        ['iG','iG','jG','lG','GC','jG','lG','kG','kG'], // GREEN / F
+        ['mR','mR','nR','pR','RC','nR','pR','oR','oR'], // RED / R
+        ['qB','qB','rB','tB','BC','rB','tB','sB','sB'], // BLUE / B
+        ['uY','uY','vY','xY','YC','vY','xY','wY','wY'], // YELLOW / D
+        // help bro pls
 ];      
 // It would be like aabbCENTERccdd
 // First is corner and second is edge
@@ -13,27 +16,30 @@ const cube = [
 // NOW WE HAVE ALL SIX SIDES OF A CUBE INDICATED BY COLORS
 let cubePlaying = [...cube];
 const cwMap = [2,5,8,1,4,7,0,3,6];
-let newSide;
+printCube(cube)
+console.log('\n' + '-'.repeat(process.stdout.columns) + '\n');
 
 function moveR(prime, doubleMove, move) {
 
-        let upp = getTheInput(prime, move);
+        let { targetArray: upp, targetSideArray: sideArray } = getTheInput(prime, move);
         let times = doubleMove ? 2 : 1;
         let { stickers, side }= sideStickers(move);
         
         for(let t = 0; t < times; t++) {
+                let newSide = [...cubePlaying[side]];
                 for(let i = 0; i < upp.length - 1; i++) {
                         for(const idx of stickers) {
                                 [cubePlaying[upp[i]][idx],     cubePlaying[upp[i + 1]][idx]] =
                                 [cubePlaying[upp[i + 1]][idx], cubePlaying[upp[i]][idx]];
                         }
                 }
-                cubePlaying[side].forEach((sticker, i) => {
-                        if (i === 4) return;
-                        newSide[cwMap[i]] = sticker;
-                })
+                cubePlaying[side].forEach((sticker, n) => {
+                        if (n === 4) return;
+                        newSide[sideArray[n]] = sticker;
+                });
+                cubePlaying[side] = newSide;
         }
-        console.log(cubePlaying)
+        printCube(cubePlaying);
 }
 function doubleArray(arr) {
         return arr = [...arr, ...arr];
@@ -42,6 +48,8 @@ function getTheInput(prime, move) {
         let R = [5, 4, 0, 2]; 
         let L = [5, 2, 0, 4]; 
         let U = [2, 3, 4, 1]; // this is the order that we are doing it in
+        let D = [2, 1, 4, 3];
+        let F = [5, 3, 0, 1];
         let targetArray, targetSideArray; 
 
         switch (move) {
@@ -54,6 +62,12 @@ function getTheInput(prime, move) {
                 case "U":
                         targetArray = U;
                         break;
+                case "D":
+                        targetArray = D;
+                        break;
+                case "F":
+                        targetArray = F;
+                        break;
                 default:
                         throw new Error(`Unknown move: "${move}"`);
         }
@@ -61,9 +75,12 @@ function getTheInput(prime, move) {
         if (prime) { // A check to see if it's not prime, as current order does a prime move
                      // Will addapt this to make it able to do 2 moves later  
                 targetArray.reverse();
+                targetSideArray = cwMap.reverse();
+        } else {
+                targetSideArray = cwMap;
         }
 
-        return targetArray;
+        return { targetArray, targetSideArray };
 }
 function sideStickers(move) {
         let stickers, side;
@@ -81,13 +98,21 @@ function sideStickers(move) {
                         stickers = [0,1,2];
                         side = 0;
                         break;
+                case "D":
+                        stickers = [6,7,8];
+                        side = 5;
+                        break;
+                case "F":
+                        stickers = [6,7,8];
+                        side = 2; // fuck why does f and b moves have to change EO...
+                        break;
                 default:
                         throw new Error(`Unknown move: "${move}"`);
         }
 
         return { stickers, side };
 }
-moveR(false, false, "R");
+moveR(true, true, "D");
 //moveR(false, false, "R");
 
 // oh no i can do old pochman lol
--- JS/utils.js
+++ JS/utils.js
@@ -1,3 +1,30 @@
 export function wait(ms) {
         return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+export function printCube(cube) {
+        const face = (i) => {
+                const f = cube[i];
+                return [
+                        [f[0], f[1], f[2]],
+                        [f[3], f[4], f[5]],
+                        [f[6], f[7], f[8]],
+                ];
+        };
+
+        const row = (faces, r) => faces.map(f => f[r].join(' ')).join('   ');
+
+        const [U, L, F, R, B, D] = [0,1,2,3,4,5].map(face);
+
+        console.log('      ' + row([U], 0));
+        console.log('      ' + row([U], 1));
+        console.log('      ' + row([U], 2));
+        console.log();
+        console.log(row([L, F, R, B], 0));
+        console.log(row([L, F, R, B], 1));
+        console.log(row([L, F, R, B], 2));
+        console.log();
+        console.log('      ' + row([D], 0));
+        console.log('      ' + row([D], 1));
+        console.log('      ' + row([D], 2));
+}
